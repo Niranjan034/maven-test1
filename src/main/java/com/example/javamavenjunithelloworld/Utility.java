a0d1276af06b860d0db6f2363a5aa31620e8ee24
Niranjan034 <niranjanreddykandula7843@gmail.com>
2025-11-10 21:18:28 +0530

    Niranjan0001- Wrote the code to find biggest among 3 numbers

--- src/main/java/com/example/javamavenjunithelloworld/Utility.java
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -3,17 +3,26 @@ package com.example.javamavenjunithelloworld;
 public class Utility {
 
     public static void main(String[] args) {
-        int a = 20;
-        if (a % 2 == 0) {
-            System.out.println("Given number is an Even Number");
-        } else {
-            System.out.println("Given number is an Odd Number");
-        }
+        int a = 25;
+        int b = 60;
+        int c = 45;
+
+        int biggest = findBiggest(a, b, c);
+        System.out.println("The biggest number among " + a + ", " + b + ", and " + c + " is: " + biggest);
 
         Niranjan();
     }
 
+    public static int findBiggest(int a, int b, int c) {
+        if (a >= b && a >= c)
+            return a;
+        else if (b >= a && b >= c)
+            return b;
+        else
+            return c;
+    }
+
     public static void Niranjan() {
-        System.out.println("I am Venkata Niranjan Reddy Kandula");
+        System.out.println("I am Niranjan Reddy Kandula");
     }
 }
