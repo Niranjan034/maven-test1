3fcfd42d7389b46f223f793106c0183d53d629b4
Niranjan034 <niranjanreddykandula7843@gmail.com>
2025-11-10 20:55:09 +0530

    Odd Or Even Number

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,19 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+
+    public static void main(String[] args) {
+        int a = 20;
+        if (a % 2 == 0) {
+            System.out.println("Given number is an Even Number");
+        } else {
+            System.out.println("Given number is an Odd Number");
+        }
+
+        Niranjan();
+    }
+
+    public static void Niranjan() {
+        System.out.println("I am Niranjan Reddy Kandula");
+    }
+}
